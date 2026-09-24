114de18d851ff30bfd70a6c9370953e53ca67ec0
Alireza.khandehjam <khandehjamalireza1@gmail.com>
2026-09-24 17:02:50 +0330

    user_pass.py

--- user_pass.py
+++ user_pass.py
@@ -2,21 +2,26 @@
 
 
 
-user_name = input("please enter your user name:")
-password = input("now please enter your password:")
-age = int(input("now please enter your age:"))
-if user_name == "Alirezakhandehjam" :
-    print("user name is correct" , "✔")
-else :
-    print("user name is not correct" , "❌")
-if password == "hamine" :
-    print("also your password is correct" , "✔")
-else :
-    print("Oops your password is not correct" , "❌")
-if 18<age<=90 :
-    if 18<age<=50 :
-        print("you are allow to enter")
-    else :
-        print("damn you look yonger" , "😉")
-else :
-        print("you are not old enough or too old to enter" , "😂")
+user_name = input("Please enter your user name: ")
+password = input("Now please enter your password: ")
+age = int(input("Now please enter your age: "))
+if user_name == "Alirezakhandehjam":
+    print("User name is correct", "✔")
+else:
+    print("User name is not correct", "❌")
+if password == "hamine":
+    print("Also your password is correct", "✔")
+else:
+    print("Oops, your password is not correct", "❌")
+if 18 < age <= 90:
+    if 18 < age <= 50:
+        print("You are allowed to enter", "✔")
+    else:
+        print("Damn, you look younger", "😉")
+else:
+    print("You are not old enough or too old to enter", "😂")
+# Final check
+if user_name == "Alirezakhandehjam" and password == "hamine" and 18 < age <= 50:
+    print("Access granted! Welcome 🎉")
+else:
+    print("Access denied! ❌")
